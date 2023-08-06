b8efe1c03d1afb7e5b2d8cbc7f603b1f40fe43c0
kerry <kerry91warnes@gmail.com>
2023-08-06 21:42:22 +0100

    first commit

---
+++ Employee/MainWindow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnSaveBtnClicked(object sender, EventArgs e)
+    {
+        this.messageEntry.Text = "Save button pushed";
+    }
+
+    protected void OnClearBtnClicked(object sender, EventArgs e)
+    {
+        this.firstEntry.Text = "";
+        this.lastEntry.Text = "";
+        this.salaryEntry.Text = "";
+        this.messageEntry.Text = "Transaction cancelled";
+    }
+}
